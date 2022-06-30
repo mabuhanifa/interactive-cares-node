b3412578dedd2c95c5578846bfd4c376c0f96eb3
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-30 11:25:46 +0600

    server created

--- index.js
+++ index.js
@@ -2,3 +2,24 @@
 
 
 */
+
+const http = require("http");
+
+const app = {};
+
+app.config = {
+  port: 3000,
+};
+
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`listening on ${app.config.port}`);
+  });
+};
+
+app.handleReqRes = (req, res) => {
+  res.end("hello world!");
+};
+
+app.createServer();
